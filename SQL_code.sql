578b0a210b00823cc5f831c43d984442432287e0
Vasily3107 <vasilynelis@gmail.com>
2025-03-26 19:34:39 +0200

    sql code

---
+++ SQL_code.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE test_db;
+USE test_db;
+
+
+CREATE TABLE users (
+--  column:             type:                    constraints:
+    username            VARCHAR(256)             PRIMARY KEY,
+    [password]          VARBINARY(256)
+);
+
+
+CREATE TABLE decryption_data (
+--  column:             type:                    constraints:
+    username            VARCHAR(256)             FOREIGN KEY REFERENCES users(username),
+    aes_key             VARBINARY(256)           NOT NULL,
+    nonce               VARBINARY(256)           NOT NULL
+);
